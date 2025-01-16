fe7497f58e836f012ba873d73a985f26d2f2d0a4
Renan <108879679+RenanAderneF@users.noreply.github.com>
2025-01-16 09:32:54 -0300

    Finalização do conteúdo das páginas, e preparação para primeira hospedagem

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Seleção de elementos: */
+
+const news_section = document.getElementById("news_section");
+const areas_section = document.getElementById("area_wrapper");
+
+/* Carrosel de notícias: */
+
+document.addEventListener("click", (e) => {
+
+    if(e.target.id == "news_section") {
+
+        alert("notícias fi");
+    }
+
+})
